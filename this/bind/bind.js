dc8ac696d46eeabd61be5ec3b8443407b5ed30c1
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-16 09:30:44 -0300

    apply, call e bind

---
+++ this/bind/bind.js
@@ -0,0 +1,17 @@
+//Bind
+//bind=ligar
+//Retorna a função chamada, porém, ligada ao contexto que lhe foi passado.
+//bound function
+this.name = "Gabriel";
+this.age = 18;
+
+function sayName (age){
+    this.age = arguments[0];
+    console.log(this.name, this.age);
+}
+
+const dev = {
+    name: "Avelino"
+}
+
+sayName.bind(dev)(19)
